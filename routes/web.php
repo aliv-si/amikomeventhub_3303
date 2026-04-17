379b02f771b6592acfe5668a39ed85a9a8bdc203
aliv-si <aliv@students.amikom.ac.id>
2026-04-17 09:58:04 +0700

    feat: update blade view layout module template

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,37 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
-Route::get('/profil', function () {
-    return view('profil');
+Route::get('/event-detail', function () {
+    return view('event-detail');
 });
 
-Route::get('/katalog', function () {
-    return view('katalog');
+Route::get('/checkout', function () {
+    return view('checkout');
 });
 
-Route::get('/bantuan', function () {
-    return view('bantuan');
+Route::get('/ticket', function () {
+    return view('ticket');
 });
 
-Route::get('/kontak', function () {
-    return view('kontak');
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
 });
+
+// Route::get('/profil', function () {
+//     return view('profil');
+// });
+
+// Route::get('/katalog', function () {
+//     return view('katalog');
+// });
+
+// Route::get('/bantuan', function () {
+//     return view('bantuan');
+// });
+
+// Route::get('/kontak', function () {
+//     return view('kontak');
+// });
